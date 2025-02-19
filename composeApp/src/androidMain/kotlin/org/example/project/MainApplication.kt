7ae85252267a68ab696dd38eda10287d5ff1b426
jafetesc98 <jafet_esc98@hotmail.com>
2025-02-19 11:23:07 -0600

    first commit 1

---
+++ composeApp/src/androidMain/kotlin/org/example/project/MainApplication.kt
@@ -0,0 +1,25 @@
+package org.example.project;
+
+import android.app.Application
+import android.util.Log
+import com.expenseApp.db.AppDatabase
+import org.example.project.data.DatabaseDriverFactory
+import org.example.project.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MainApplication)
+            androidLogger()
+            modules(appModule(AppDatabase.invoke(DatabaseDriverFactory(this@MainApplication).createDriver())))
+        }
+
+    }
+}
+
+
